e2c68b25af502700fd744d6b24ab58bf251dc106
LorenzoAbbadessa <lor.abbadessa@gmail.com>
2023-12-17 19:08:12 +0100

    finisce esercizio

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php $mess = "ciao numero 1" ?>
+</head>
+<body>
+    <h1><?php echo "$mess"?></h1>
+    <h2><?php echo "ciao numero 2"?></h2>
+</body>
+</html>
